fdc009ff58044081826ac0853909161dfc0d24e4
Hiroshi Yoshida <hrysd22@gmail.com>
2015-09-10 15:32:40 +0900

    Initialize commit

---
+++ index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const App           = require('app'),
+      BrowserWindow = require('browser-window'),
+      Menubar       = require('menubar')({dir: __dirname, width: 980, height: 850});
